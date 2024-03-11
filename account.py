d930cb2d1176fff136a77c724bf93e0d7b54288c
Salt-bucket <saltbucket0@gmail.com>
2024-03-11 13:19:29 +0100

    Account Class

--- account.py
+++ account.py
@@ -1,2 +1,14 @@
-print("Micheal")
-print("11-3-2024")
+class Account:
+
+    def __init__(self, fname, lname, phone_no, passcode, email,
+                 bvn, pin):
+        self.__fname = fname
+        self.__lname = lname
+        self.__phone_no = phone_no
+        self.__email = email
+        self.__pin = pin
+        self.__passcode = passcode
+        self.__address = None
+        self.__balance = 0.0
+        self.__bvn = bvn
+        self.nin = None
