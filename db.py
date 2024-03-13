cfd3d0fe9fa4c2633bf77b67b7561f55ec89e7bf
Salt-bucket <saltbucket0@gmail.com>
2024-03-13 10:49:04 +0100

    Created two new files

--- db.py
+++ db.py
@@ -1,14 +1,15 @@
 from account import Account
-customers:dict = dict()
+db:dict = dict()
 
-account1 = Account(fname = "John", lname = "Doe", email = "john.doe@email.com", passcode = "12345", pin = "1234")
+account1 = Account(fname = "John", lname = "Doe", email = "john.doe@email.com",
+                   passcode = "12345", pin = "1234")
 account1.set_address("101 Spooner Street, Quahog")
 account1.set_phone_no("08000000000")
 
-account2 = Account(fname = "Jane", lname = "Smith", email = "jane.smith@email.com", passcode = "54321", pin = "4321")
+account2 = Account(fname = "Jane", lname = "Smith", email = "jane.smith@email.com",
+                   passcode = "54321", pin = "4321")
 account2.set_address("503 Cover Drive, Quahog")
 account2.set_phone_no("08000000000")
 
-
-customers[account1.get_account_no()] = account1
-customers[account2.get_account_no()] = account2
+db[account1.get_account_no()] = account1
+db[account2.get_account_no()] = account2
