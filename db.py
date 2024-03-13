a5c2cff3f3bbdbe1486efe53d0be7f333da4209b
Salt-bucket <saltbucket0@gmail.com>
2024-03-13 10:32:00 +0100

    Created db module

--- db.py
+++ db.py
@@ -5,9 +5,9 @@ account1 = Account(fname = "John", lname = "Doe", email = "john.doe@email.com", 
 account1.set_address("101 Spooner Street, Quahog")
 account1.set_phone_no("08000000000")
 
-account1 = Account(fname = "Jane", lname = "Smith", email = "jane.smith@email.com", passcode = "54321", pin = "4321")
-account1.set_address("503 Cover Drive, Quahog")
-account1.set_phone_no("08000000000")
+account2 = Account(fname = "Jane", lname = "Smith", email = "jane.smith@email.com", passcode = "54321", pin = "4321")
+account2.set_address("503 Cover Drive, Quahog")
+account2.set_phone_no("08000000000")
 
 
 customers[account1.get_account_no()] = account1
